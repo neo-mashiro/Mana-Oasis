f1f93a02b8e9bdf7affbf72ce713f8cbc25ecee1
neo-mashiro <neo-mashiro@hotmail.com>
2020-12-21 08:03:33 -0500

    C# 8.0 switch expression

---
+++ Assets/Scripts/Utilities/Commuter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utilities {
+    
+    /// <summary>
+    /// Simulates smooth recurring translation or rotation movements between two positions.
+    /// </summary>
+    public class Commuter : MonoBehaviour {
+        
+        private enum Motions {Translate, Rotate};
+        private enum Axes {X, Y, Z}
+
+        [SerializeField] private Motions motion = Motions.Translate;
+        [SerializeField] private Axes axis = Axes.X;
+        [SerializeField] private float speed = 1.0f, magnitude = 1f;
+
+        private Vector3 _direction;
+
+        private void Update() {
+            // c# 8.0 switch expression feature not yet supported in Unity
+            // _direction = axis switch {
+            //     Axes.X => Vector3.right,
+            //     Axes.Y => Vector3.up,
+            //     Axes.Z => Vector3.forward,
+            //     _ => throw new ArgumentOutOfRangeException()
+            // };
+
+            switch (axis) {
+                case Axes.X:
+                    _direction = Vector3.right;
+                    break;
+                case Axes.Y:
+                    _direction = Vector3.up;
+                    break;
+                case Axes.Z:
+                    _direction = Vector3.forward;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            if (motion == Motions.Translate) {
+                transform.Translate(_direction * (Mathf.Cos(Time.time * speed) * magnitude * speed * 0.01f));
+            }
+            else {
+                transform.Rotate(_direction * (Time.deltaTime * speed * Mathf.Rad2Deg));
+            }
+        }
+    }
+}
